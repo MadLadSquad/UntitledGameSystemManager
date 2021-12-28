664d2efdf31c55dc590ea547120d725c0d018d97
Madman10K <contact@madman10k.online>
2021-12-28 15:25:39 +0200

    Saving submodule changes(Build will fail!)

--- Common/GUI/Window.cpp
+++ Common/GUI/Window.cpp
@@ -1,5 +1,29 @@
-//
-// Created by i-use-gentoo-btw on 12/28/21.
-//
-
 #include "Window.hpp"
+#include <glfw3.h>
+
+UGM::GUI::Window::~Window()
+{
+    destroy();
+}
+
+void UGM::GUI::Window::init()
+{
+    if (!glfwInit())
+    {
+        logger.consoleLog("GLFW initialization failed!", UVK_LOG_TYPE_ERROR);
+        glfwTerminate();
+        return;
+    }
+
+}
+
+void UGM::GUI::Window::destroy()
+{
+
+}
+
+void UGM::GUI::Window::framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    //auto* windowInst = static_cast<Window*>(glfwGetWindowUserPointer(window));
+    glViewport(0, 0, width, height);
+}
--- Common/GUI/Window.hpp
+++ Common/GUI/Window.hpp
@@ -1,1 +1,22 @@
 #pragma once
+#include <Core.hpp>
+
+struct GLFWwindow;
+namespace UGM::GUI
+{
+    class Window
+    {
+    public:
+        Window() = default;
+        Window(const Window&) = delete;
+        void operator=(Window const&) = delete;
+        ~Window();
+
+        void init();
+    private:
+        void destroy();
+
+        static void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+        GLFWwindow* windowMain = nullptr;
+    };
+}
