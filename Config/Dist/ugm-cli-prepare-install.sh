d86e356bc863e45f90781cec0316ba676a922611
Stanislav Vasilev <stanv.contact176@gmail.com>
2024-02-25 13:14:45 +0200

    Update ugm-cli-prepare-install.sh

--- Config/Dist/ugm-cli-prepare-install.sh
+++ Config/Dist/ugm-cli-prepare-install.sh
@@ -5,7 +5,7 @@ else
   read -rp "Enter the name of your regular user: " user
 
   while true; do
-      echo -e "Make sure that you have lxd and lxc installed as well as that you're running this as root!"
+      echo -e "Make sure that you have incus and lxc installed as well as that you're running this as root!"
       read -rp "Start installation? Y(Yes)/N(No): " yn
       case $yn in
           [Yy]* ) break;;
@@ -15,8 +15,8 @@ else
   done
 fi
 
-# Enable lxd daemon
-rc-update add lxd default || systemctl enable lxd.service
+# Enable Incus daemon
+rc-update add incus default || systemctl enable incus.service
 
 # Remove the last line from limits.conf
 sed -i '$ d' /etc/security/limits.conf
@@ -36,25 +36,25 @@ root    hard    nofile  1048576
 echo "root:1000000:1000000000" >> /etc/subuid
 echo "root:1000000:1000000000" >> /etc/subgid
 
-# Add the user listed by the user to the lxd group
-usermod -a -G lxd "${user}"
+# Add the user listed by the user to the incus group
+usermod -a -G incus incus-admin "${user}"
 
-# Start the lxd service
-/etc/init.d/lxd start || systemctl start lxd.service
+# Start the incus service
+/etc/init.d/incus start || systemctl start incus.service
 
 su "${user}" -c "(xhost +local: | grep 'access control disabled' || xhost +local:) && exit"
 
 # This complicated line checks if the needed string is there, if it is continue, if it's not add it, restart pulseaudio and continue
 (grep "load-module module-native-protocol-unix auth-anonymous=1" /etc/pulse/default.pa &> /dev/null && echo -e "\x1B[32mPulseAudio already installed!\x1B[0m") || (sed -i "s/load-module module-native-protocol-unix/& auth-anonymous=1/" /etc/pulse/default.pa && su "${user}" -c "killall pulseaudio && pulseaudio &> /dev/null & disown && exit")
 
-# Start lxd init with everything being default with the exception of the storage backend
-lxd init --auto --storage-backend=dir
+# Start incus init with everything being default with the exception of the storage backend
+incus init --auto --storage-backend=dir
 # Disable IPv6 because it messes up some configurations' network connectivity
 lxc network set lxdbr0 ipv6.nat=false
 lxc network set lxdbr0 ipv6.address=none
 
-# Restart LXD so that the network changes can take effect
-/etc/init.d/lxd restart || systemctl restart lxd.service
+# Restart Incus so that the network changes can take effect
+/etc/init.d/incus restart || systemctl restart incus.service
 
 echo -e "\x1B[32mPre-install finished successfully!\x1B[0m"
 echo -e "\x1B[32mAdd the following path to your PATH environment variable in order for scripts to work: ~/.config/UntitledLinuxGamingManager/scripts/\x1B[0m"
